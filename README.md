fbd4b4e020ab3cefbcf04c6fd3e10f5851c07378
unknown <kieto@ualbert.ca>
2019-04-21 21:14:50 -0400

    Finished Data Sanitization

---
+++ README.md
@@ -0,0 +1,14 @@
+# Columbia Data Sanitzation
+
+## Goal
+> Filter Value 64 from xlsx based datasets into a single file
+
+## How to Run
+>run pip install -r requirements.txt
+>close all instances of microsoft excel
+>run python index.py
+>observe the results in Table.xlsx file
+
+## Technologies Used
+* Python 
+* xlwings
--- index.py
+++ index.py
@@ -1,33 +1,34 @@
 import xlwings as xw
 from os import walk
 
-years = ['1980-2000', '2000-2015']
+years = [
+        {'range': '1980-2000', 'idNumber': '19802000'},
+        {'range': '2000-2015', 'idNumber': '20002015'}
+        ]
 results = xw.Book('Table.xlsx')
 
-def getValue64(file, filename):
-    file = file.sheets[f'{filename}-19802000']
-    val64col = ""
-    val64row = ""
-    cols = file.range('A1:Z1')
-    rows = file.range('B2:B30')       
-    for col in cols:
+def getValue64(file, filename, idNumber):
+    sheet = file.sheets[f'{filename}-{idNumber}']
+    for col in sheet.range('N1:Z1'):
         if 'VALUE_64' in col.value:
             val64col = col.address[1]
             break
-    for row in rows:
+    for row in sheet.range('B10:B30'):
         if 64 == row.value:
             val64row = row.address[3::]
             break
-    return file.range(f'{val64col}{val64row}').value
+    return sheet.range(f'{val64col}{val64row}').value
 
 def inputData(year):
     currentRow = 3
-    for (dirpath, dirnames, filenames) in walk(f'./{year}'):
-        currentYear = results.sheets[f'{year}']
+    yearRange = year['range']
+    idNumber = year['idNumber']
+    for (dirpath, dirnames, filenames) in walk(f'./{yearRange}'):
+        currentYear = results.sheets[f'{yearRange}']
         for dir in dirnames:
             print(dir)
-            currentFile = xw.Book(f'{year}/{dir}/{dir}-19802000.xlsx')
-            currentYear.range(f'R{currentRow}').value = getValue64(currentFile, dir)
+            currentFile = xw.Book(f'{yearRange}/{dir}/{dir}-{idNumber}.xlsx')
+            currentYear.range(f'R{currentRow}').value = getValue64(currentFile, dir, idNumber)
             currentRow += 1
             currentFile.close()
 
